53506b1f8f805966fc24c591a091257e73eb9705
Unknown <prabalg@gmail.com>
2018-01-16 17:10:36 -0500

    Added unit tests

---
+++ tests/Flitesys.Utilities.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace Flitesys.Utilities.Tests
+{
+    public class ObjectExtensionsTests
+    {
+        [Fact]
+        public void ObjectIsNull()
+        {
+            string isNotNull = "This is not null";
+            string isNull = null;
+
+            Assert.True(isNull.IsNull());
+            Assert.False(isNotNull.IsNull());
+        }
+
+        [Fact]
+        public void ObjectIsNotNull()
+        {
+            string isNotNull = "This is not null";
+            string isNull = null;
+
+            Assert.False(isNull.IsNotNull());
+            Assert.True(isNotNull.IsNotNull());
+        }
+    }
+}
