955b5788f69f27ade088383bd43d20fdb19a1ff7
Prabal Ghosh <prabalg@gmail.com>
2018-01-18 14:53:47 -0500

    Upgraded to GeographicLib 1.0.1

--- src/Flitesys.Utilities/GeographicPoint.cs
+++ src/Flitesys.Utilities/GeographicPoint.cs
@@ -5,40 +5,47 @@ using Flitesys.GeographicLib;
 
 namespace Flitesys.Utilities
 {
-	public class GeographicPoint
-	{
-		public double Latitude { get; set; }
-		public double Longitude { get; set; }
+    public struct GeographicPoint
+    {
+        public double Latitude;
+        public double Longitude;
 
-		public GeographicPoint()
-		{ }
+        public GeographicPoint(double lat, double lon)
+        {
+            Latitude = lat;
+            Longitude = lon;
+        }
 
-		public GeographicPoint(double lat, double lon)
-		{
-			Latitude = lat;
-			Longitude = lon;
-		}
+        public GeographicPoint(GeographicPoint another)
+        {
+            Latitude = another.Latitude;
+            Longitude = another.Longitude;
+        }
 
-		public GeographicPoint(GeographicPoint another)
-		{
-			Latitude = another.Latitude;
-			Longitude = another.Longitude;
-		}
+        /// <summary>
+        /// Computes the distance, initial course and final course from this point to another point
+        /// </summary>
+        /// <param name="toPoint">Destination geographic coordinates</param>
+        /// <returns>A GeographicResult object giving the distance, initial course and final course</returns>
+        public GeographicResult DistanceTo(GeographicPoint toPoint)
+        {
+            GeodesicData g = Geodesic.WGS84.Inverse(this.Latitude, this.Longitude, toPoint.Latitude, toPoint.Longitude, GeodesicMask.ALL);
+            return new GeographicResult
+            {
+                Distance = g.Distance,
+                InitialCourse = g.InitialAzimuth < 0 ? 360.0 + g.InitialAzimuth : g.InitialAzimuth,
+                FinalCourse = g.FinalAzimuth < 0 ? 360.0 + g.FinalAzimuth : g.FinalAzimuth
+            };
+        }
 
-		/// <summary>
-		/// Computes the distance, initial course and final course from this point to another point
-		/// </summary>
-		/// <param name="toPoint">Destination geographic coordinates</param>
-		/// <returns>A GeographicResult object giving the distance, initial course and final course</returns>
-		public GeographicResult DistanceTo(GeographicPoint toPoint)
-		{
-			GeodesicData g = Geodesic.WGS84.Inverse(this.Latitude, this.Longitude, toPoint.Latitude, toPoint.Longitude, GeodesicMask.ALL);
-			return new GeographicResult
-			{
-				Distance = g.s12,
-				InitialCourse = g.azi1 < 0 ? 360.0 + g.azi1 : g.azi1,
-				FinalCourse = g.azi2 < 0 ? 360.0 + g.azi2 : g.azi2
-			};
-		}
-	}
+        public GeographicPoint DestinationPoint(double course, double distance)
+        {
+            GeodesicData g = Geodesic.WGS84.Direct(this.Latitude, this.Longitude, course, distance);
+            return new GeographicPoint
+            {
+                Latitude = g.Latitude2,
+                Longitude = g.Longitude2
+            };
+        }
+    }
 }
