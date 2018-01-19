0c216a13fdf35e97ece0851d228da92280f5881c
Prabal Ghosh <prabalg@gmail.com>
2018-01-19 11:08:42 -0500

    Added FlightDate class

---
+++ src/Flitesys.Utilities/FlightDate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flitesys.Utilities
+{
+    public class FlightDate
+    {
+        private DateTime _dateTime;
+        private TimeZoneInfo _timeZone;
+
+        private FlightDate()
+        {
+        }
+
+        public FlightDate(DateTime dateTime, TimeZoneInfo tzi, DateTimeKind kind)
+        {
+            this._dateTime = dateTime;
+            this._timeZone = tzi;
+        }
+
+        public FlightDate(DateTime dateTime, TimeZoneInfo tzi)
+        {
+            this._dateTime = dateTime;
+            this._timeZone = tzi;
+        }
+    }
+}
