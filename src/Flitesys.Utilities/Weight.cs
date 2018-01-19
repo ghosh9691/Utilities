27a2cbb4bcf22aaabe721f4f172e0dceb6d89c12
Prabal Ghosh <prabalg@gmail.com>
2018-01-19 13:11:10 -0500

    Added Weight and WeightUnits

---
+++ src/Flitesys.Utilities/Weight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flitesys.Utilities
+{
+    public struct Weight
+    {
+        public double Value;
+        public WeightUnits Units;
+
+        public Weight(double value, WeightUnits units)
+        {
+            this.Value = value;
+            this.Units = units;
+        }
+    }
+}
---
+++ src/Flitesys.Utilities/WeightUnits.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flitesys.Utilities
+{
+    public enum WeightUnits
+    {
+        Kilogram,
+        Pound
+    }
+}
