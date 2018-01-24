90cf58950a188061bf5b97f8a559097ceda8ac9c
Prabal Ghosh <prabalg@gmail.com>
2018-01-24 10:48:01 -0500

    Added Json formatting for WeightUnits

--- src/Flitesys.Utilities/Weight.cs
+++ src/Flitesys.Utilities/Weight.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +9,8 @@ namespace Flitesys.Utilities
     public struct Weight
     {
         public double Value;
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public WeightUnits Units;
 
         public Weight(double value, WeightUnits units)
