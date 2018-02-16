ad27aff31b50ffa682983904f0fb06c114d0d62d
Prabal Ghosh <prabalg@outlook.com>
2018-02-16 09:33:23 -0500

    Added id and pnr generators

---
+++ src/Flitesys.Utilities/IdGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Flitesys.Utilities
+{
+    public class IdGenerator
+    {
+        public static string GetId(int length = 16)
+        {
+            string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            return RandomString(length, allowedCharacters);
+        }
+
+        public static string GetPNR(int length = 6)
+        {
+            string allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            return RandomString(length, allowedCharacters);
+        }
+
+        private static string RandomString(int length, string alphabet)
+        {
+            var outOfRange = byte.MaxValue + 1 - (byte.MaxValue + 1) % alphabet.Length;
+
+            return string.Concat(
+                Enumerable
+                .Repeat(0, int.MaxValue)
+                .Select(e => RandomByte())
+                .Where(randomByte => randomByte < outOfRange)
+                .Take(length)
+                .Select(randomByte => alphabet[randomByte % alphabet.Length])
+                );
+        }
+
+        private static byte RandomByte()
+        {
+            using (var randomizer = RandomNumberGenerator.Create())
+            {
+                var randomBytes = new byte[1];
+                randomizer.GetBytes(randomBytes);
+                return randomBytes.Single();
+            }
+        }
+    }
+}
---
+++ tests/Flitesys.Utilities.Tests/IdGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Flitesys.Utilities.Tests
+{
+    public class IdGeneratorTests
+    {
+        [Fact]
+        public void TestGetId()
+        {
+            var generated = IdGenerator.GetId();
+            Assert.NotNull(generated);
+            Assert.True(generated.Length == 16);
+        }
+
+        [Fact]
+        public void TestGetPNR()
+        {
+            var pnr = IdGenerator.GetPNR();
+            Assert.NotNull(pnr);
+            Assert.True(pnr.Length == 6);
+        }
+    }
+}
