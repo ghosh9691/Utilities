1066e18753fe9c74892cbd542b7168be1971687b
Prabal Ghosh <prabalg@outlook.com>
2018-03-16 13:33:27 -0400

    Added TZDatabase class to fetch list of IANA timezones

---
+++ src/Flitesys.Utilities/TZDatabase.cs
@@ -0,0 +1,18 @@
+﻿using NodaTime.TimeZones;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Flitesys.Utilities
+{
+    public static class TZDatabase
+    {
+        public static List<string> GetTimeZones()
+        {
+            var result = new List<string>();
+            TzdbDateTimeZoneSource.Default.ZoneLocations.ToList().ForEach(tz => { result.Add(tz.ZoneId); });
+            return result;
+        }
+    }
+}
