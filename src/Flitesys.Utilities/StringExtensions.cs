5922033e40553a27c96fdcab504dfb13d1bdce48
Prabal Ghosh <prabalg@outlook.com>
2018-04-02 10:35:36 -0400

    Added to string extensions

--- src/Flitesys.Utilities/StringExtensions.cs
+++ src/Flitesys.Utilities/StringExtensions.cs
@@ -41,14 +41,72 @@ namespace Flitesys.Utilities
             return Encoding.UTF8.GetString(decoded);
         }
 
-		public static bool IsEmpty(this string value)
-		{
-			return string.IsNullOrEmpty(value);
-		}
+        public static bool IsEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
 
-		public static bool IsNotEmpty(this string value)
-		{
-			return !string.IsNullOrEmpty(value);
-		}
+        public static bool IsNotEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static double FromAiracLatitude(this string value)
+        {
+            if (value.Length != 9)
+                throw new ArgumentException($"{value} is not an AIRAC Latitude!");
+            if ((value[0] != 'N') && (value[0] != 'S'))
+                throw new ArgumentException($"{value} does not have hemisphere information!");
+            try
+            {
+                int deg = Convert.ToInt32(value.Substring(1, 2));
+                int min = Convert.ToInt32(value.Substring(3, 2));
+                int sec = Convert.ToInt32(value.Substring(5, 2));
+                int hSec = Convert.ToInt32(value.Substring(7, 2));
+                double target = deg + min / 60.0 + sec / 3600.0 + hSec / 360000.0;
+                if (value[0] == 'S')
+                    target = -target;
+                return target;
+            }
+            catch
+            {
+                return -91.0;
+            }
+        }
+
+        public static double FromAiracLongitude(this string value)
+        {
+            if (value.Length != 10)
+                throw new ArgumentException($"{value} is not an AIRAC Longitude!");
+            if ((value[0] != 'E') && (value[0] != 'W'))
+                throw new ArgumentException($"{value} does not have hemisphere information!");
+            try
+            {
+                int deg = Convert.ToInt32(value.Substring(1, 3));
+                int min = Convert.ToInt32(value.Substring(4, 2));
+                int sec = Convert.ToInt32(value.Substring(6, 2));
+                int hSec = Convert.ToInt32(value.Substring(8, 2));
+                double target = deg + min / 60.0 + sec / 3600.0 + hSec / 360000.0;
+                if (value[0] == 'W')
+                    target = -target;
+                return target;
+            }
+            catch
+            {
+                return -181.0;
+            }
+        }
+
+        public static DateTime AiracEffectiveDate(this string value)
+        {
+            var airac = new Airac(value);
+            return airac.GetEffectiveDate();
+        }
+
+        public static DateTime AiracDiscontinueDate(this string value)
+        {
+            var airac = new Airac(value);
+            return airac.GetDiscontinueDate();
+        }
     }
 }
