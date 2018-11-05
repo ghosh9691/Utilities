909c32ebd063292a13692497bdb9a0c017b1e1db
Prabal Ghosh <prabalg@outlook.com>
2018-11-05 13:55:44 -0500

    Added method to configure global exception handling

---
+++ src/Flitesys.AirCloud.Core/ExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using Flitesys.Utilities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace Flitesys.AirCloud.Core
+{
+    public class ExceptionHandler
+    {
+        private static RequestDelegate ExceptionResponseHandler()
+        {
+            return async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature.IsNotNull())
+                {
+                    await context.Response.WriteAsync(new
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = contextFeature.Error.Message
+                    }.ToJson());
+                }
+            };
+        }
+    }
+}
