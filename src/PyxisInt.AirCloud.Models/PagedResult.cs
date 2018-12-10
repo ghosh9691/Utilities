fb521f9ee0f4e0302f78e8c6a00a3ca7d7cb477a
Prabal Ghosh <prabalg@outlook.com>
2018-12-10 10:14:26 -0500

    Fix build

--- src/PyxisInt.AirCloud.Models/PagedResult.cs
+++ src/PyxisInt.AirCloud.Models/PagedResult.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace PyxisInt.AirCloud.Models
 {
-    public class PagedResult
+    public class PagedResult<T> : PagedResultBase where T : class
     {
+        public IList<T> Results { get; set; }
+
         public PagedResult()
         {
+            Results = new List<T>();
         }
     }
 }
--- src/PyxisInt.AirCloud.Models/Result.cs
+++ src/PyxisInt.AirCloud.Models/Result.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace PyxisInt.AirCloud.Models
 {
-    public class Result<T>
+    public class Result<T> where T: class
     {
         public T Data { get; set; }
         public Error Error { get; set; }
