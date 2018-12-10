134377e5eeccc9ebf06ebfd92d8d7fb0d7a17972
Prabal Ghosh <prabalg@outlook.com>
2018-12-10 10:10:59 -0500

    Added PagedResult classes

---
+++ src/PyxisInt.AirCloud.Models/Error.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PyxisInt.AirCloud.Models
+{
+    public class Error
+    {
+        public Error()
+        {
+        }
+    }
+}
---
+++ src/PyxisInt.AirCloud.Models/Result.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace PyxisInt.AirCloud.Models
+{
+    public class Result<T>
+    {
+        public T Data { get; set; }
+        public Error Error { get; set; }
+    }
+}
