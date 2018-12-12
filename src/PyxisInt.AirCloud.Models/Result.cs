ecdc2db4caf72b91fdea6dea7dc13aaa59ff0b2c
Prabal Ghosh <prabalg@outlook.com>
2018-12-12 14:27:15 -0500

    Added constructors to Result

--- src/PyxisInt.AirCloud.Models/Result.cs
+++ src/PyxisInt.AirCloud.Models/Result.cs
@@ -5,5 +5,29 @@ namespace PyxisInt.AirCloud.Models
     {
         public T Data { get; set; }
         public Error Error { get; set; }
+
+        public Result()
+        {
+
+        }
+
+        public Result(T data)
+        {
+            Data = data;
+        }
+
+        public Result(string errorCode, string errorDescription)
+        {
+            Error = new Error { 
+                ErrorCode = errorCode,
+                ErrorDescription = errorDescription
+            };
+        }
+
+        public Result(T data, Error error)
+        {
+            Data = data;
+            Error = error;
+        }
     }
 }
