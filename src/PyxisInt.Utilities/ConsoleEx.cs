99d6f8a73a97df08389a11f6e555ef5a51ed19da
Prabal Ghosh <prabalg@outlook.com>
2018-12-20 12:11:56 -0500

    Added ConsoleEx

---
+++ src/PyxisInt.Utilities/ConsoleEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PyxisInt.Utilities
+{
+    public static class ConsoleEx
+    {
+        public static void WriteMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void WriteError(string msg)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = color;
+        }
+
+        public static void WriteWarning(string msg)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = color;
+        }
+    }
+}
