0a00b9d2403f95b26423163694246eb8e3fbef80
Prabal Ghosh <prabalg@outlook.com>
2019-04-03 16:26:01 -0400

    Added Operational Airports

--- src/PyxisInt.AirCloud.Models/Navigation/OperationalAirport.cs
+++ src/PyxisInt.AirCloud.Models/Navigation/OperationalAirport.cs
@@ -3,8 +3,10 @@ namespace PyxisInt.AirCloud.Models.Navigation
 {
     public class OperationalAirport
     {
-        public OperationalAirport()
-        {
-        }
+        public Guid AirportId { get; set; }
+        public Airport Airport { get; set; }
+
+        public Guid AirlineId { get; set; }
+        public Airline Airline { get; set; }
     }
 }
