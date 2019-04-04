5621437ca2c78ec5bfaf62fa9437126236f22b0f
Prabal Ghosh <prabalg@outlook.com>
2019-04-04 11:20:51 -0400

    Added AirportTypes

--- src/PyxisInt.AirCloud.Models/Navigation/OperationalAirport.cs
+++ src/PyxisInt.AirCloud.Models/Navigation/OperationalAirport.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using PyxisInt.AirCloud.Models.Enums;
 namespace PyxisInt.AirCloud.Models.Navigation
 {
     public class OperationalAirport
     {
-        public Guid AirportId { get; set; }
-        public Airport Airport { get; set; }
-
+        [JsonProperty("airlineId")]
+        [Required]
         public Guid AirlineId { get; set; }
-        public Airline Airline { get; set; }
+
+        [JsonProperty("airportId")]
+        [Required]
+        public Guid AirportId { get; set; }
+
+        [JsonProperty("timeZone")]
+        [StringLength(50)]
+        public string TimeZone { get; set; }
+
+        [JsonProperty("airportType")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        [Required]
+        public AirportTypes AirportType { get; set; }
+
+        [JsonProperty("city")]
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        [StringLength(50)]
+        public string Country { get; set; }
     }
 }
