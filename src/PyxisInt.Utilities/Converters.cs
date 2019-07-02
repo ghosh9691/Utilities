e01b6083975c3068a59de3af460bf10f5d373102
Prabal Ghosh <prabalg@outlook.com>
2019-07-02 14:01:20 -0400

    Added exception safe convert method

--- src/PyxisInt.Utilities/Converters.cs
+++ src/PyxisInt.Utilities/Converters.cs
@@ -1,6 +1,31 @@
-﻿namespace PyxisInt.Utilities
+﻿using System;
+
+namespace PyxisInt.Utilities
 {
     public class Converters
     {
+        public static int ToInt32(string val)
+        {
+            try
+            {
+                return Convert.ToInt32(val);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static double ToDouble(string val)
+        {
+            try
+            {
+                return Convert.ToDouble(val);
+            }
+            catch
+            {
+                return 0.0;
+            }
+        }
     }
 }
