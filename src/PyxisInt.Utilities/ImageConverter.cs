07eff168e6db7c30f49b8c9f4a84490bf5cf7e2a
Prabal Ghosh <prabalg@outlook.com>
2019-08-29 14:24:52 -0400

    Added Image Converter

---
+++ src/PyxisInt.Utilities/ImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace PyxisInt.Utilities
+{
+    public class ImageConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var base64 = (string)reader.Value;
+            // convert base64 to byte array, put that into memory stream and feed to image
+            return Image.FromStream(new MemoryStream(Convert.FromBase64String(base64)));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var image = (Image)value;
+            // save to memory stream in original format
+            var ms = new MemoryStream();
+            image.Save(ms, image.RawFormat);
+            byte[] imageBytes = ms.ToArray();
+            // write byte array, will be converted to base64 by JSON.NET
+            writer.WriteValue(imageBytes);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Image);
+        }
+    }
+}
