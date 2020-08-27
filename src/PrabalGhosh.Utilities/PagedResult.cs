d0b338046a5dbdb91fdcea265393dcd6b6334824
Prabal Ghosh <prabal.ghosh@precisely.com>
2020-08-27 11:37:45 -0400

    Added PagedResult class

---
+++ src/PrabalGhosh.Utilities/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PrabalGhosh.Utilities
+{
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public IList<T> Results { get; set; }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+
+    }
+}
---
+++ src/PrabalGhosh.Utilities/PagedResultBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PrabalGhosh.Utilities
+{
+    public class PagedResultBase
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+
+        public int FirstRowOnPage
+        {
+            get { return (CurrentPage - 1) * PageSize + 1; }
+        }
+
+        public int LastRowOnPage
+        {
+            get { return Math.Min(CurrentPage * PageSize, RowCount); }
+        }
+    }
+}
