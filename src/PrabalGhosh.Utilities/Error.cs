d17aefc858be000f43c43f9b35f19b39be21c8f2
Prabal Ghosh <prabal.ghosh@precisely.com>
2020-08-27 12:55:08 -0400

    Added Result class - useful for a consistent return from APIs

---
+++ src/PrabalGhosh.Utilities/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace PrabalGhosh.Utilities
+{
+    public class Error
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+}
---
+++ src/PrabalGhosh.Utilities/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace PrabalGhosh.Utilities
+{
+    public class Result<T> where T : class
+    {
+        public T Data { get; set; }
+        public Error? Error { get; set; }
+
+        public Result()
+        {
+
+        }
+
+        public Result(T data)
+        {
+            Data = data;
+        }
+
+        public Result(string errorCode, string errorDescription)
+        {
+            Error = new Error
+            {
+                ErrorCode = errorCode,
+                ErrorDescription = errorDescription
+            };
+        }
+
+        public Result(T data, Error error)
+        {
+            Data = data;
+            Error = error;
+        }
+    }
+}
