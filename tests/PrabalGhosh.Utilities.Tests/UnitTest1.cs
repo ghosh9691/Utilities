57e8074b9833db071f1bb28cdf1cbf61f7cdd1fb
Prabal Ghosh <prabalg@outlook.com>
2021-04-24 11:54:26 -0400

    Closes 131
    Switched to MSTest

---
+++ tests/PrabalGhosh.Utilities.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrabalGhosh.Utilities.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
